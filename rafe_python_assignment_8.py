8644016af51801d52e30fe157dab65c2094f9fb0
rafe_stefano <stefanorafe@gmail.com>
2020-05-13 22:47:09 +0300

    assignment8_after modify

--- rafe_python_assignment_8.py
+++ rafe_python_assignment_8.py
@@ -1,6 +1,6 @@
 # rafe_python_assignment_8
 
-year= int(input("Please enter a name to check if it's leap year or not: "))
+year= int(input("Please enter a year to check if it's leap year or not: "))
 
 if (year%400==0) or (year%100!=0) and (year%4==0):
     print(f"{year} is a leap year.")
